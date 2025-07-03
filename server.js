89650bed6ae5f996b160789c724072589f5e77be
cylian charbonnier <cylian.charbonnier1@gmail.com>
2025-07-03 18:44:55 +0200

    add env example

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+require("dotenv").config();
 const http = require('http');
 const app = require('./app');
+const { exit } = require('process');
 
 app.set('port', 4000);
 global.baseDir = __dirname;
 
+if (process.env.DBaccess === "" || process.env.TokenKey === "") {
+    throw new Error("Env variable not set")
+}
+console.log(process.env.DBAccess)
 const server = http.createServer(app);
 
 server.listen(4000);
